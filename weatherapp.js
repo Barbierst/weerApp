dcd701f2979c381ddfc94d5a72844082530ac7fb
Tim Barbiers <42208814+Barbierst@users.noreply.github.com>
2019-05-10 20:58:00 +0200

    Add files via upload

--- weatherapp.js
+++ weatherapp.js
@@ -6,13 +6,19 @@ window.addEventListener('load', ()=> {
     let locationTimezone = document.querySelector('.timezone-name');
     let temperatureSection = document.querySelector('.temperature');
     let temperatureSpan = document.querySelector('.temperature span');
+    let temperatureDescriptionTomorrow = document.querySelector('.temperature-description-tomorrow');
+    let temperatureDegreeTomorrow = document.querySelector('.temperature-degree-tomorrow');
+    let locationTimezoneTomorrow = document.querySelector('.timezone-name-tomorrow');
+    let temperatureSectionTomorrow = document.querySelector('.temperature-tomorrow');
+    let temperatureSpanTomorrow = document.querySelector('.temperature-tomorrow span');
+
 
     if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
-            const proxy = `http://cors-anywhere.herokuapp.com/`;
+            const proxy = `https://cors-anywhere.herokuapp.com/`;
             const api = `${proxy}https://api.darksky.net/forecast/71e236e715c3efa449c4da1640c85346/${lat},${long}`;
 
             fetch(api)
@@ -20,12 +26,15 @@ window.addEventListener('load', ()=> {
                 return response.json();
             })
                 .then(data => {
+                    console.log(data);
                 const { temperature, summary, icon } = data.currently;
+        
                 //DOM elements veranderen in inhoud van de API
 
                 temperatureDegree.textContent = temperature;
                 temperatureDescription.textContent = summary;
                 locationTimezone.textContent = data.timezone;
+                locationTimezoneTomorrow.textContent = data.timezone;
 
                     //Formule voor Celsius
                     let celsius = (temperature - 32) * (5/9);
@@ -44,9 +53,39 @@ window.addEventListener('load', ()=> {
                         }
                     })
             })
+
+            fetch(api)
+                .then(response => {
+                return response.json();
+            })
+                .then(data => {
+
+                const { apparentTemperatureHigh, summary, icon } = data.daily.data["1"];
+
+                temperatureDegreeTomorrow.textContent = apparentTemperatureHigh;
+                temperatureDescriptionTomorrow.textContent = summary;
+
+                    //Formule voor Celsius
+                    let celsius = (apparentTemperatureHigh - 32) * (5/9);
+
+                    //set icon
+                    setIcons(icon, document.querySelector('.icon-tomorrow'));
+
+                    //verander temperatuur naar celsius
+                    temperatureSectionTomorrow.addEventListener('click', () => {
+                        if (temperatureSpanTomorrow.textContent === 'F') {
+                            temperatureSpanTomorrow.textContent = 'C';
+                            temperatureDegreeTomorrow.textContent = Math.floor(celsius);
+                        } else {
+                            temperatureSpanTomorrow.textContent = 'F';
+                            temperatureDegreeTomorrow.textContent = apparentTemperatureHigh;
+                        }
+                    })
+
+                })
         });
     } else {
-        document.querySelector('.timezone-name').textContent = 'Om het weer te bekijken, moet je geolocatie aan staan!';
+        document.querySelector('.timezone-name').textContent = 'Om het weer te bekijken hebben moet je geolocatie aan staan!';
     }
 
     function setIcons(icon, iconID) {
